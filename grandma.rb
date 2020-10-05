1d4ced49f517c376a0e225399fe7e106f6201b5a
Michael Kim <m.kim4247@gmail.com>
2020-10-05 17:57:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(phrase)
     "HUH?!, SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
-  else phrase == "I LOVE YOU GRANDMA"
+  else phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   end
 end
